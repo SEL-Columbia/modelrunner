975092c8525ff3374c245cf74df86eb528865185
chrisnatali <chris.natali@gmail.com>
2014-09-14 21:31:20 -0400

    let py script run from scripts dir

--- scripts/mvmax_sequence.sh
+++ scripts/mvmax_sequence.sh
@@ -5,4 +5,6 @@ output_dir=$2
 
 echo "reading input from $input_dir"
 source activate sequencer
-python mvmax_sequencer.py -i $input_dir -o $output_dir
+# Get source dir and run py script from there
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+python $DIR/mvmax_sequencer.py -i $input_dir -o $output_dir
