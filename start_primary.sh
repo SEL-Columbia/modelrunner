2204f32667cb7e488bfd0943477095e4faef8fd1
chrisnatali <chris.natali@gmail.com>
2014-09-22 17:59:14 -0400

    in support of travis-ci

---
+++ start_primary.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# start all processes in support of primary server in background
+# assumes model_runner conda environment has been activated
+nohup python job_server.py --port=8080 --worker_is_primary=False > job_server.log 2>&1 & echo $! > job_server.pid
+nohup python -m SimpleHTTPServer 8000 > primary_static_server.log 2>&1 & echo $! > primary_static_server.pid
+nohup python job_primary.py --worker_is_primary=False > job_primary.log 2>&1 & echo $! > job_primary.pid
+
