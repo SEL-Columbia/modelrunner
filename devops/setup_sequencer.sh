f7f4307978c5d6460000f6a8c7b295719332869c
chrisnatali <chris.natali@gmail.com>
2014-10-16 18:32:19 -0400

    remove all versions of sequencer prior to install

--- devops/setup_sequencer.sh
+++ devops/setup_sequencer.sh
@@ -5,6 +5,10 @@ conda config --set always_yes yes --set changeps1 no
 # legacy cleanup, shouldn't be needed soon
 rm -rf $HOME/miniconda/conda-bld/linux-64/sequencer-*.bz2
 
+# I believe this is needed if we re-upload sequencer and
+# don't change the version in the meta.yaml
+rm -rf $HOME/miniconda/pkgs/sequencer*
+
 echo "Setup Sequencer env"
 rm -rf $HOME/miniconda/envs/sequencer
 conda create -n sequencer python=2.7
