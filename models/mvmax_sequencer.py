8600f4c407306333edfcc7bf82f322cb5dda939e
chrisnatali <chris.natali@gmail.com>
2015-08-18 13:24:14 -0400

    updating sequencer to do logging

--- models/mvmax_sequencer.py
+++ models/mvmax_sequencer.py
@@ -5,11 +5,20 @@ Run Sequencer with specific model
 
 import argparse
 import os
-
+import sys
+import logging
+import sequencer
 from sequencer import NetworkPlan
 from sequencer.Models import EnergyMaximizeReturn
 
 if __name__ == '__main__':
+    # setup log
+    logger = logging.getLogger('sequencer')
+
+    logger.info("sequencer %s (Python %s)" % (
+                    sequencer.__version__,
+                    '.'.join(map(str, sys.version_info[:3]))))
+
     parser = argparse.ArgumentParser(
                 description="Run Sequencer on MVMax derived scenario")
     parser.add_argument("-i", "--input-directory", default="data/input",
