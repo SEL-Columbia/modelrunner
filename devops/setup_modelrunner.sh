6e3da38789e659b304fd211478ff0ae161200ffa
chrisnatali <chris.natali@gmail.com>
2016-09-19 18:19:24 -0400

    setup conda env with python 3

--- devops/setup_modelrunner.sh
+++ devops/setup_modelrunner.sh
@@ -6,5 +6,5 @@ source $HOME/.bash_profile
 rm -rf $HOME/miniconda/envs/modelrunner
 
 conda config --set always_yes yes --set changeps1 no
-conda create -n modelrunner python=2.7
+conda create -n modelrunner python=3
 conda install --yes -c sel -n modelrunner modelrunner
