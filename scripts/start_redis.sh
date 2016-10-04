bbc07e2cf13edec26f3a40be541d055fec5bab73
chrisnatali <chris.natali@gmail.com>
2016-10-04 15:24:07 -0400

    closes #95

--- scripts/start_redis.sh
+++ scripts/start_redis.sh
@@ -4,7 +4,7 @@
 # assumes modelrunner conda environment has been activated
 if [ -e redis.conf ]
 then
-    redis-server redis.conf > redis.log 2>&1 & echo $! > redis.pid 
+    redis-server redis.conf > redis.log 2>&1 &
 else
-    redis-server > redis.log 2>&1 & echo $! > redis.pid 
+    redis-server > redis.log 2>&1 &
 fi
