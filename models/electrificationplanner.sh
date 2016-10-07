7cfd6331ab58d3f64fe8b8de9f0539d6cdac75ab
chrisnatali <chris.natali@gmail.com>
2016-10-07 15:02:07 -0400

    bug fix

--- models/electrificationplanner.sh
+++ models/electrificationplanner.sh
@@ -7,6 +7,6 @@ output_dir=$2
 echo "Running electrificationplanner"
 echo "Reading input from $input_dir"
 pushd $HOME/infrastructure-planning
-source activate infrastructure-planning
+source activate electrificationplanner
 python estimate_electricity_cost_by_technology_from_population.py $input_dir/config.json -w $input_dir -o $output_dir
 popd
