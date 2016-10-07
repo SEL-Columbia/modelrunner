658271da9bf3e65c89bd359c379fa3cbd3d9982c
chrisnatali <chris.natali@gmail.com>
2016-10-07 13:09:15 -0400

    make model setup file and name consistent

---
+++ devops/setup_electrificationplanner.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source $HOME/.bash_profile
+# so that conda doesn't exit after running
+conda config --set always_yes yes
+
+# NOTE:  the repo is named infrastructure-planning, but model is
+# referred to as electrificationplanner in modelrunner
+echo "Setup electrificationplanner env"
+# electrificationplanner requires full source to run
+rm -rf $HOME/electrificationplanner
+git clone https://github.com/SEL-Columbia/infrastructure-planning
+
+# install dependencies
+rm -rf $HOME/miniconda/envs/electrificationplanner
+conda create -n electrificationplanner python=2.7
+conda install -n electrificationplanner -c conda-forge -c sel infrastructure-planning
---
+++ devops/setup_networkplanner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source $HOME/.bash_profile
+# so that conda doesn't exit after running
+conda config --set always_yes yes
+
+echo "Setup networkplanner env"
+rm -rf $HOME/miniconda/envs/networkplanner
+conda create -n networkplanner python=2.7
+conda install -n networkplanner -c conda-forge -c sel networkplanner-metrics
+conda install -n networkplanner -c conda-forge -c sel networkplanner
